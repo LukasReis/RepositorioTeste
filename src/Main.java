61debd91e5ab13ded9560dede2b77f5e3e45aa0f
LukasReis <luke.silvareis@gmail.com>
2023-02-27 15:07:48 -0300

    Primeiro commit

--- src/Main.java
+++ src/Main.java
@@ -1,4 +1,5 @@
 public class Main {
+    //Inicio
     public static void main(String[] args) {
         System.out.println("Hello world!");
     }
--- src/br/com/dio/PrimieroPrograma.java
+++ src/br/com/dio/PrimieroPrograma.java
@@ -5,6 +5,7 @@ import br.com.dio.model.Gato;
 public class PrimieroPrograma {
     public static void main(String[] args) {
         Gato gato =  new Gato();
+        gato.setNome("café");
         System.out.print(gato);
 
 
